5257e98d5e2eec916cbdd155000a4905dbc23f07
lyx0206331 <rq880113@163.com>
2021-12-09 17:08:21 +0800

    Initial commit

---
+++ app/src/main/java/com/voocoo/smart/VoocooApp.kt
@@ -0,0 +1,41 @@
+package com.voocoo.smart
+
+import android.app.Application
+
+//                       _ooOoo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                       O\ = /O
+//                   ____/`---'\____
+//                 .   ' \\| |// `.
+//                  / \\||| : |||// \
+//                / _||||| -:- |||||- \
+//                  | | \\\ - /// | |
+//                | \_| ''\---/'' | |
+//                 \ .-\__ `-` ___/-. /
+//              ______`. .' /--.--\ `. . __
+//           ."" '< `.___\_<|>_/___.' >'"".
+//          | | : `- \`.;`\ _ /`;.`/ - ` : | |
+//            \ \ `-. \_ __\ /__ _/ .-` / /
+//    ======`-.____`-.___\_____/___.-`____.-'======
+//                       `=---='
+//
+//    .............................................
+//             佛祖保佑             永无BUG
+/**
+ * Author:RanQing
+ * Date:2021/12/9 3:06 下午
+ * Description:
+ */
+class VoocooApp: Application() {
+
+    companion object {
+        lateinit var instance: VoocooApp
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
