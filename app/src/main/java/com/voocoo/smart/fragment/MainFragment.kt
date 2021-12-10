33997f8a4953482b8ca993b86aadb66d9184e016
lyx0206331 <rq880113@163.com>
2021-12-10 18:41:25 +0800

    add sth

--- app/src/main/java/com/voocoo/smart/fragment/MainFragment.kt
+++ app/src/main/java/com/voocoo/smart/fragment/MainFragment.kt
@@ -5,56 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.voocoo.smart.R
+import com.voocoo.smart.databinding.FragmentMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val binding = FragmentMainBinding.inflate(inflater, container, false)
+        val viewPager = binding.viewPager
+        viewPager.adapter = MainViewPagerAdapter(this)
+        val tabLayout = binding.tabs
+        TabLayoutMediator(tabLayout, viewPager) { tab, posision ->
+            when(posision) {
+                FRAGMENT_HOME_PAGE_INDEX -> {
+                    tab.setIcon(R.drawable.home_tab_selector)
+                    tab.setText(R.string.home)
+                }
+                FRAGMENT_MARKET_PAGE_INDEX -> {
+                    tab.setIcon(R.drawable.home_tab_selector)
+                    tab.setText(R.string.market)
+                }
+                FRAGMENT_FORUM_PAGE_INDEX -> {
+                    tab.setIcon(R.drawable.home_tab_selector)
+                    tab.setText(R.string.forum)
+                }
+                FRAGMENT_MINE_PAGE_INDEX -> {
+                    tab.setIcon(R.drawable.home_tab_selector)
+                    tab.setText(R.string.mine)
                 }
             }
+        }.attach()
+        return binding.root
     }
+
+}
+
+const val FRAGMENT_HOME_PAGE_INDEX = 0
+const val FRAGMENT_MARKET_PAGE_INDEX = 1
+const val FRAGMENT_FORUM_PAGE_INDEX = 2
+const val FRAGMENT_MINE_PAGE_INDEX = 3
+
+class MainViewPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+
+    private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
+        FRAGMENT_HOME_PAGE_INDEX to { HomeFragment() },
+        FRAGMENT_MARKET_PAGE_INDEX to { MarketFragment() },
+        FRAGMENT_FORUM_PAGE_INDEX to { ForumFragment() },
+        FRAGMENT_MINE_PAGE_INDEX to { MineFragment() }
+    )
+
+    override fun getItemCount(): Int = tabFragmentsCreators.size
+
+    override fun createFragment(position: Int): Fragment = tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
 }
